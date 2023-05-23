7f8d316f491edb041fd16599cf644548bb88039b
Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-05-23 11:57:02 +0300

    benibul with firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,47 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
 import Add from "/home/codert/Documents/programming/benibul/src/img/addAvatar.png";
 import Register from "./Register";
 
+// 
+
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase";
+
+// 
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const Login = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Beni Bul</span>
         <span className="title">Giris yap</span>
-        <form>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <button> Giris yap</button>
+        <form onSubmit={Login}>
+          <input 
+          type="email" placeholder="email"  
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}/>
+          <input 
+          type="password" placeholder="password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}/>
+          <button type="submit"> Giris yap</button>
         </form>
         <p>
           Hesabiniz mi yok? <Link to="/register">Kaydol</Link>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,23 +1,51 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
 import Add from "/home/codert/Documents/programming/benibul/src/img/addAvatar.png";
+// 
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase";
 
+// 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const Register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
+  
+  
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Beni Bul</span>
         <span className="title">Kaydol</span>
-        <form>
-          <input type="text" placeholder="display name" />
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
+        <form onSubmit={Register}>
+          <input 
+          type="text" placeholder="display name" />
+          <input 
+          type="email" placeholder="email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}/>
+          <input 
+          type="password" placeholder="password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}/>
           <input style={{ display: "none" }} type="file" id="file" />
           <label htmlFor="file">
             <img src={Add} alt="" />
             <span>Profil ekle</span>
           </label>
-          <button>Sign up</button>
+          <button type="submit">Sign up</button>
         </form>
         <p>
           Hesabin mi var? <Link to="/login">Giris yap</Link>
